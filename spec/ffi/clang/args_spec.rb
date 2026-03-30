d80db39a263a8c686b85afbc9828a1082f52baf9
Charlie Savage <cfis@savagexi.com>
2026-03-29 19:57:11 -0600

    More rubocop happiness.

--- spec/ffi/clang/args_spec.rb
+++ spec/ffi/clang/args_spec.rb
@@ -7,21 +7,21 @@ RSpec.describe FFI::Clang::Args do
 		it "runs llvm-config from PATH" do
 			original_llvm_config = ENV["LLVM_CONFIG"]
 			original_llvm_version = ENV["LLVM_VERSION"]
-
+			
 			ENV.delete("LLVM_CONFIG")
 			ENV.delete("LLVM_VERSION")
-
+			
 			args = described_class.new
 			status = double(success?: true)
-
+			
 			expect(Open3).to receive(:capture3)
 				.with("llvm-config", "--libdir")
 				.and_return(["/opt/llvm/lib\n", "", status])
-
+			
 			expect(args.send(:llvm_library_dir)).to eq("/opt/llvm/lib")
 		ensure
 			ENV["LLVM_CONFIG"] = original_llvm_config
-
+			
 			if original_llvm_version
 				ENV["LLVM_VERSION"] = original_llvm_version
 			else
